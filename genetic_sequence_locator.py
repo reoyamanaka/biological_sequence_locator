677f990d2e7933dec8371c8c4a5d2b9936946cd4
Reo Yamanaka <ryamanaka807@gmail.com>
2020-09-04 16:05:14 -0400

    Added wireframe

--- genetic_sequence_locator.py
+++ genetic_sequence_locator.py
@@ -1,4 +1,4 @@
-import wx
+import wx, os, sys, wx.lib.scrolledpanel
 
 class MyApp(wx.App):
     def __init__(self):
@@ -8,7 +8,7 @@ class MyApp(wx.App):
 
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title = "Genetic Sequence Locator", size = (600, 600))
+        super().__init__(parent=None, title = "Genetic Sequence Locator", size = (700, 650))
         self.OnInit()
 
     def OnInit(self):
@@ -17,6 +17,35 @@ class MyFrame(wx.Frame):
 class MyPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent = parent)
+
+        labelFont = wx.Font(14, wx.DEFAULT, wx.BOLD, wx.NORMAL)
+        titleLabel = wx.StaticText(self, label = "Genetic Sequence Locator", pos = (30, 30))
+        titleLabel.SetFont(labelFont)
+
+        inputLabel = wx.StaticText(self, label = "Select the .txt file containing the full genetic sequence.", pos = (30, 75))
+        txtFileInput = wx.DirPickerCtrl(self, pos = (370, 70))
+
+        sequenceLabel = wx.StaticText(self, label = "Insert the sequence of interest.", pos = (30, 125))
+        sequenceInput = wx.TextCtrl(self, pos = (240, 120), size = (200, 28))
+        sequenceButton = wx.Button(self, label = "Search sequence of interest.", pos = (450, 125))
+
+        resultsLabel = wx.StaticText(self, label = "Results:", pos = (30, 175))
+        resultsLabel.SetFont(labelFont)
+        
+        occurrenceLabel = wx.StaticText(self, label = "Occurrence frequency of the sequence in the input .txt:", pos = (30, 215))
+        occurrence = wx.StaticText(self, label = "0", pos = (380, 215))
+
+        locationLabel = wx.StaticText(self, label = "Location(s)", pos = (30, 255))
+        locationOutput = wx.StaticText(self, label = "", pos = (30, 290))
+
+        panelScroll = wx.lib.scrolledpanel.ScrolledPanel(self, size = (500, 250), pos = (30, 290), style = wx.SIMPLE_BORDER)
+        boxSizer = wx.BoxSizer(wx.VERTICAL)
+
+        def quitProgram(self):
+            sys.exit()
+        
+        quitButton = wx.Button(self, label = "Quit", pos = (30, 560))
+        quitButton.Bind(wx.EVT_BUTTON, quitProgram)
 
 if __name__ == "__main__":
     app = MyApp()
