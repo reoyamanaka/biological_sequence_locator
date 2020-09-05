4f52afc065d5f34c5f27d452aac4b9c9bef30764
Reo Yamanaka <ryamanaka807@gmail.com>
2020-09-05 03:17:27 -0400

    started file-reading functionality

--- genetic_sequence_locator.py
+++ genetic_sequence_locator.py
@@ -1,3 +1,6 @@
+#nucleotide sequences in gene file
+#peptide sequences in the protein file
+
 import wx, os, sys, wx.lib.scrolledpanel
 
 class MyApp(wx.App):
@@ -22,23 +25,33 @@ class MyPanel(wx.Panel):
         titleLabel = wx.StaticText(self, label = "Genetic Sequence Locator", pos = (30, 30))
         titleLabel.SetFont(labelFont)
 
-        inputLabel = wx.StaticText(self, label = "Select the .txt file containing the full genetic sequence.", pos = (30, 75))
-        txtFileInput = wx.DirPickerCtrl(self, pos = (370, 70))
+        inputLabel = wx.StaticText(self, label = "Select the .txt file containing the COMPLETE sequence of interest.", pos = (30, 75))
+        txtFileInput = wx.FilePickerCtrl(self, pos = (440, 70))
+            
+        sequenceLabel = wx.StaticText(self, label = "Insert the shorter sequence of interest.", pos = (30, 125))
+        sequenceInput = wx.TextCtrl(self, pos = (280, 123), size = (200, 24))
 
-        sequenceLabel = wx.StaticText(self, label = "Insert the sequence of interest.", pos = (30, 125))
-        sequenceInput = wx.TextCtrl(self, pos = (240, 120), size = (200, 28))
-        sequenceButton = wx.Button(self, label = "Search sequence of interest.", pos = (450, 125))
+        def locate(self):
+            fileLocation = sequenceInput.GetValue()
+            appendInput = ""
+            with open(fileLocation, 'r') as rf:
+                for line in rf:
+                    appendInput += line
+            print(appendInput)
+
+        sequenceButton = wx.Button(self, label = "Search sequence of interest.", pos = (490, 125))
+        sequenceButton.Bind(wx.EVT_BUTTON, locate)
 
         resultsLabel = wx.StaticText(self, label = "Results:", pos = (30, 175))
         resultsLabel.SetFont(labelFont)
         
-        occurrenceLabel = wx.StaticText(self, label = "Occurrence frequency of the sequence in the input .txt:", pos = (30, 215))
-        occurrence = wx.StaticText(self, label = "0", pos = (380, 215))
+        occurrenceLabel = wx.StaticText(self, label = "Occurrence frequency of the shorter sequence in the input .txt:", pos = (30, 215))
+        occurrence = wx.StaticText(self, label = "", pos = (380, 215))
 
-        locationLabel = wx.StaticText(self, label = "Location(s)", pos = (30, 255))
+        locationLabel = wx.StaticText(self, label = "Location(s):", pos = (30, 255))
         locationOutput = wx.StaticText(self, label = "", pos = (30, 290))
 
-        panelScroll = wx.lib.scrolledpanel.ScrolledPanel(self, size = (500, 250), pos = (30, 290), style = wx.SIMPLE_BORDER)
+        panelScroll = wx.lib.scrolledpanel.ScrolledPanel(self, size = (650, 250), pos = (30, 290), style = wx.SIMPLE_BORDER)
         boxSizer = wx.BoxSizer(wx.VERTICAL)
 
         def quitProgram(self):
@@ -46,6 +59,7 @@ class MyPanel(wx.Panel):
         
         quitButton = wx.Button(self, label = "Quit", pos = (30, 560))
         quitButton.Bind(wx.EVT_BUTTON, quitProgram)
+
 
 if __name__ == "__main__":
     app = MyApp()
