ca483ea2ab657e2d0aecdfe1381981c9c31b859c
Reo Yamanaka <ryamanaka807@gmail.com>
2020-09-06 04:43:58 -0400

    completed core functionality

--- biological_sequence_locator.py
+++ biological_sequence_locator.py
@@ -1,7 +1,4 @@
-#nucleotide sequences in gene file
-#peptide sequences in the protein file
-
-import wx, os, sys, wx.lib.scrolledpanel
+import wx, os, sys, wx.lib.scrolledpanel, re
 
 class MyApp(wx.App):
     def __init__(self):
@@ -22,11 +19,18 @@ class MyPanel(wx.Panel):
         super().__init__(parent = parent)
 
         labelFont = wx.Font(14, wx.DEFAULT, wx.BOLD, wx.NORMAL)
-        titleLabel = wx.StaticText(self, label = "Genetic Sequence Locator", pos = (30, 30))
+        titleLabel = wx.StaticText(self, label = "Biological Sequence Locator", pos = (30, 30))
         titleLabel.SetFont(labelFont)
 
         inputLabel = wx.StaticText(self, label = "Select the .txt file containing the COMPLETE sequence.", pos = (30, 75))
         txtFileInput = wx.FilePickerCtrl(self, pos = (370, 70))
+
+        panelScroll = wx.lib.scrolledpanel.ScrolledPanel(self, size = (650, 250), pos = (30, 290), style = wx.SIMPLE_BORDER)
+        locationsPrimer = wx.StaticText(panelScroll, label = "The shorter sequence of interest was found on:", pos = (20, 9))
+        locationsPrimer.Hide()
+        locations = wx.StaticText(panelScroll, label = "", pos = (250, 65))
+        boxSizer = wx.BoxSizer(wx.VERTICAL)
+
             
         sequenceLabel = wx.StaticText(self, label = "Insert the shorter sequence of interest.", pos = (30, 125))
         sequenceInput = wx.TextCtrl(self, pos = (280, 123), size = (200, 24))
@@ -42,12 +46,22 @@ class MyPanel(wx.Panel):
             if appendInput.find(soi) == -1:
                 print("not found")
             else:
-                print(appendInput.find(soi))
-
-            
-            
-            
-
+                locationsPrimer.Show()
+                panelScroll.SetSizer(boxSizer)
+                panelScroll.SetupScrolling(scroll_x = True, scroll_y = True)
+                matches = re.finditer(soi, appendInput)
+                raw_matches_positions = [match.start() for match in matches]
+                matches_positions = []
+                for i in raw_matches_positions:
+                    matches_positions.append(i + 1)
+                occurrence.SetLabel(str(len(matches_positions)))
+                print(matches_positions)
+                tempLocations = 2*"\n"
+                for i in matches_positions:
+                    tempLocations += "\t" + "Nucleotide/amino acid index: " + str(i) + "\n"
+                boxSizer.Add(locations)
+                locations.SetLabel(tempLocations)
+                
         sequenceButton = wx.Button(self, label = "Search Sequence of Interest", pos = (490, 125))
         sequenceButton.Bind(wx.EVT_BUTTON, locate)
 
@@ -55,13 +69,11 @@ class MyPanel(wx.Panel):
         resultsLabel.SetFont(labelFont)
         
         occurrenceLabel = wx.StaticText(self, label = "Occurrence frequency of the shorter sequence in the input .txt:", pos = (30, 215))
-        occurrence = wx.StaticText(self, label = "", pos = (380, 215))
+        occurrence = wx.StaticText(self, label = "", pos = (430, 215))
 
         locationLabel = wx.StaticText(self, label = "Location(s):", pos = (30, 255))
         locationOutput = wx.StaticText(self, label = "", pos = (30, 290))
 
-        panelScroll = wx.lib.scrolledpanel.ScrolledPanel(self, size = (650, 250), pos = (30, 290), style = wx.SIMPLE_BORDER)
-        boxSizer = wx.BoxSizer(wx.VERTICAL)
 
         def quitProgram(self):
             sys.exit()
@@ -69,8 +81,6 @@ class MyPanel(wx.Panel):
         quitButton = wx.Button(self, label = "Quit", pos = (30, 560))
         quitButton.Bind(wx.EVT_BUTTON, quitProgram)
 
-
 if __name__ == "__main__":
     app = MyApp()
     app.MainLoop()
-        
